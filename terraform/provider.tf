68d81fab05eb87387ad15dbfcc6965b1e7fef5f7
Pavel Pomortsev <100894372+omega-pasha@users.noreply.github.com>
2023-08-13 16:36:38 +0700

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,6 +9,6 @@ terraform {
 provider "yandex" {
     zone = "ru-central1-a"
     service_account_key_file = "key.json"
-    cloud_id = "b1g8dacglmlq7d7hed97"
-    folder_id = "b1glodb3fcb408dgl593"
+    cloud_id = ""
+    folder_id = ""
 }
